8a050a787d0153f6219f4bcaf5ed04097edcf265
thegermanblob <thegermanblob@gmail.com>
2021-01-22 16:58:51 +0000

    updated to follow betty guide lines

--- temp.c
+++ temp.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-
-int main()
+/**
+*main - well its main
+*Return: the usual check
+*/
+int main(void)
 {
 	int fahr, cels;
-	int lower , upper, step;
+	int lower, upper, step;
+
 	lower = 0;
 	upper = 300;
 	step = 20;
-	
 	fahr = lower;
-	while (fahr <= upper){
-	cels = 5*(fahr - 32)/9;
+	while (fahr <= upper)
+	{
+	cels = 5 * (fahr - 32) / 9;
 
-	printf("%d\t%d\n",fahr,cels);
-	fahr =fahr + step;
+	printf("%d\t%d\n", fahr, cels);
+	fahr = fahr + step;
 	}
 	puts("hi");
-	return 0;
+	return (0);
 }
